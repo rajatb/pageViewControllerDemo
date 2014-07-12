a2348e2ed258203601d6c2a894a14b8ac31238fa
Rajat Bhargava <rajat.bhargava@gmail.com>
2014-07-12 15:45:04 -0700

    Initial Commit

---
+++ pageViewControllerDeom/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pageViewControllerDeom
+//
+//  Created by Bhargava, Rajat on 7/12/14.
+//  Copyright (c) 2014 welldone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pageViewControllerDeom/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pageViewControllerDeom
+//
+//  Created by Bhargava, Rajat on 7/12/14.
+//  Copyright (c) 2014 welldone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
